c759610c7cf86c958bd96f341e8690ac092e6d43
JhonHTipas21 <168132558+JhonHTipas21@users.noreply.github.com>
2026-08-28 08:47:14 -0500

    feat(domain): define SensorData entity for real-time readings

---
+++ src/domain/entities/SensorData.ts
@@ -0,0 +1,5 @@
+export interface SensorData {
+  humidity: number; // Porcentaje de humedad: 0% a 100%
+  temperature: number; // Temperatura ambiente en Celsius
+  timestamp: Date; // Estampa de tiempo de la lectura
+}
